9cb3bcc7405f3e45106c543f6fcf0ef36a6021fa
HaraT0323 <alra9si916323@gmail.com>
2022-12-05 00:10:22 +0900

    transaction送信時のsignature署名処理を実装

--- post_transaction.py
+++ post_transaction.py
@@ -1,26 +1,64 @@
 import requests
 import json
 import datetime
+import binascii
+
+from ecdsa import SigningKey
+from ecdsa import SECP256k1
 
 
-def main():
+def main(
+    secret_key_str, sender_pub_key_str, receiver_pub_key_str, amount, description, url
+):
     time = datetime.datetime.now().isoformat()
+
+    transaction_unsigned = {
+        "time": time,
+        "sender": sender_pub_key_str,
+        "receiver": receiver_pub_key_str,
+        "amount": amount,
+        "description": description,
+    }
+
+    secret_key = SigningKey.from_string(
+        binascii.unhexlify(secret_key_str), curve=SECP256k1
+    )
+    signature_str = signature(transaction_unsigned, secret_key)
 
     transaction = {
         "time": time,
-        "sender": "Yamada",
-        "receiver": "Sato",
-        "amount": 999,
-        "description": "text",
-        "signature": "sample",
+        "sender": sender_pub_key_str,
+        "receiver": receiver_pub_key_str,
+        "amount": amount,
+        "description": description,
+        "signature": signature_str,
     }
 
-    url = "https://1gf72q.deta.dev/transaction_pool/"
-    # url = "https://s3c4k7.deta.dev/transaction_pool/"
     res = requests.post(url, json.dumps(transaction))
 
     print(res.json())
 
 
+def signature(transaction_unsigned, secret_key):
+    transaction_json = json.dumps(transaction_unsigned)
+    transaction_bytes = bytes(transaction_json, encoding="utf-8")
+    signature = secret_key.sign(transaction_bytes)
+    signature_str = signature.hex()
+    return signature_str
+
+
 if __name__ == "__main__":
-    main()
+    url = "http://127.0.0.1:8002/transaction_pool/"
+    secret_key_str = ""
+    sender_pub_key_str = ""
+    receiver_pub_key_str = ""
+    amount = 222
+    description = "test"
+    main(
+        secret_key_str,
+        sender_pub_key_str,
+        receiver_pub_key_str,
+        amount,
+        description,
+        url,
+    )
